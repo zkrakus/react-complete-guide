6c904458acd2269796e6281b938b40fc0f6cd3d3
zkrakus <zkrakus@gmail.com>
2023-05-21 11:29:27 -0400

    Added ExpenseInput component.

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,6 +1,6 @@
 import './ExpenseDate.css'
 
-export default function ExpenseDate(props) {
+const ExpenseDate = (props) => {
   const month = props.date.toLocaleString("en-US", { month: "long" });
   const day = props.date.toLocaleString("en-US", { day: "2-digit" });
   const year = props.date.getFullYear();
@@ -13,3 +13,5 @@ export default function ExpenseDate(props) {
     </div>
   );
 }
+
+export default ExpenseDate;
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,7 +2,7 @@ import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
-export default function ExpenseItem(props) {
+ const ExpenseItem = (props) => {
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -10,6 +10,9 @@ export default function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button>Change Title</button>
     </Card>
   );
 }
+
+export default ExpenseItem;
