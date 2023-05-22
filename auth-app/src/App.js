910aab9876ce819380596eb75d5c8417462dfd27
zkrakus <zkrakus@gmail.com>
2023-05-22 16:16:03 -0400

    Added onLogoutHandler to ctx as well.

--- auth-app/src/App.js
+++ auth-app/src/App.js
@@ -28,7 +28,7 @@ function App() {
   };
 
   return (
-    <AuthContext.Provider value={{isLoggedIn: isLoggedIn}}>
+    <AuthContext.Provider value={{isLoggedIn: isLoggedIn, onLogout : logoutHandler}}>
       <MainHeader onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
