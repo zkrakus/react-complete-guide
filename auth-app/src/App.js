ee454a4ffedf92354d60ca5328dc796ff9832e20
zkrakus <zkrakus@gmail.com>
2023-05-22 16:10:28 -0400

    Added context to manage isLoggedIn state between components.

--- auth-app/src/App.js
+++ auth-app/src/App.js
@@ -1,25 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
+import AuthContext from "./components/store/auth-context";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  
-  useEffect(() => {
-    const storedUserLoggedInInformation = localStorage.getItem('isLoggedin');
 
-    if(storedUserLoggedInInformation === '1'){
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedin");
+
+    if (storedUserLoggedInInformation === "1") {
       setIsLoggedIn(true);
     }
-
   }, []);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
-    localStorage.setItem('isLoggedIn', '1')
+    localStorage.setItem("isLoggedIn", "1");
     setIsLoggedIn(true);
   };
 
@@ -28,13 +28,13 @@ function App() {
   };
 
   return (
-    <React.Fragment>
+    <AuthContext.Provider value={{isLoggedIn: isLoggedIn}}>
       <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </AuthContext.Provider>
   );
 }
 
