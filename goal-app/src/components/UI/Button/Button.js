b58349d8fe555dd95933bd845eb8632ddf97dff3
zkrakus <zkrakus@gmail.com>
2023-05-21 20:30:30 -0400

    Styled button using css modules.

--- goal-app/src/components/UI/Button/Button.js
+++ goal-app/src/components/UI/Button/Button.js
@@ -1,39 +1,39 @@
-import styled from "styled-components";
-
+// import styled from "styled-components";
+import styles from './Button.module.css'
 // the double back tic is a tag template literal
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
-const Button = styled.button`
-  width: 100$;
-  font: inherit;
-  padding: 0.5rem 1.5rem;
-  border: 1px solid #8b005d;
-  color: white;
-  background: #8b005d;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-  cursor: pointer;
+// const Button = styled.button`
+//   width: 100$;
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
 
-  @media (min-width: 768px){
-    width: auto;
-  }
+//   @media (min-width: 768px){
+//     width: auto;
+//   }
   
-  &:focus {
-    outline: none;
-  }
+//   &:focus {
+//     outline: none;
+//   }
 
-  &:hover,
-  &:active {
-    background: #ac0e77;
-    border-color: #ac0e77;
-    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-  }
-`;
+//   &:hover,
+//   &:active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+// `;
 
-// const Button = props => {
-//   return (
-//     <button type={props.type} className="button" onClick={props.onClick}>
-//       {props.children}
-//     </button>
-//   );
-// };
+const Button = props => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
