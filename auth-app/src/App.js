220cdc7da260090243d7fb56e57ff74cb2ae0c81
zkrakus <zkrakus@gmail.com>
2023-05-22 16:12:49 -0400

    Used context hook instead.

--- auth-app/src/App.js
+++ auth-app/src/App.js
@@ -29,7 +29,7 @@ function App() {
 
   return (
     <AuthContext.Provider value={{isLoggedIn: isLoggedIn}}>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <MainHeader onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
