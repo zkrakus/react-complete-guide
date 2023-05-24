f5ae3b198b7a026cf815405f818c4714bd672e9b
zkrakus <zkrakus@gmail.com>
2023-05-23 21:01:55 -0400

    Did some work with react optimizatoins.

--- react-optimization/src/App.js
+++ react-optimization/src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 
 import './App.css';
 
+import Button from './components/UI/Button'
+import DemoOutput from './components/Demo/DemoOutput';
+
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false)
+  const [allowToggle, setAllowToggle] = useState(false)
+
+  console.log("APP RUNNING")
+
+  const toggleParagraphHandler = useCallback(() => {
+    if(allowToggle){
+      setShowParagraph((prevShowParagraph) = !prevShowParagraph)
+    }
+  }, [allowToggle])
+
+  const allowTogglehandler = () => {
+    setAllowToggle(true);
+  }
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <DemoOutput show={showParagraph}/>
+      <Button onClick={allowTogglehandler}>Allow Toggling</Button>
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
 }
