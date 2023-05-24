f28c86d8b4aca251f5d989e2a9085764517b2aca
zkrakus <zkrakus@gmail.com>
2023-05-23 22:57:46 -0400

    Custom hook initial commit.

---
+++ custom-hook/src/hooks/use-counter.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from "react";
+
+const useCounter = (forwards = true) => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (forwards) {
+        setCounter((prev) => prev + 1);
+      } else {
+        setCounter((prev) => prev - 1);
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [forwards]);
+
+  return counter;
+};
+
+export default useCounter;
