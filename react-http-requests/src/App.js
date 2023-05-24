c72c06b1f77cc9b67332d629351f1f2a408bc1b6
zkrakus <zkrakus@gmail.com>
2023-05-24 10:35:39 -0400

    Cleanup

--- react-http-requests/src/App.js
+++ react-http-requests/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -8,16 +8,12 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  // useEffect(() => {
-  //   fetchMovieHandler();
-  // }, [])
-
-  const fetchMovieHandler = async () => {
+  const fetchMovieHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
 
     try {
-      const response = await fetch("https://swapi.dev/api/film/");
+      const response = await fetch("https://swapi.dev/api/films/");
       if (!response.ok) {
         throw new Error("Something went wrong!!");
       }
@@ -38,19 +34,23 @@ function App() {
     }
 
     setIsLoading(false);
-  };
+  },[]);
+
+  useEffect(() => {
+    fetchMovieHandler();
+  }, [fetchMovieHandler])
 
   let content = <p>Found no movies.</p>
-  if(movies.Length > 0) {
+  if(movies.length > 0) {
     content = <MoviesList movies={movies} />
   }
 
   if(error) {
-    <p>{error}</p>
+    content = <p>{error}</p>
   }
 
   if(isLoading){
-    isLoading && <p>Loading...</p>
+    content = <p>Loading...</p>
   }
 
   return (
